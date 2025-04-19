7b55ae0df1ac72c3a5077a85ed5027649aacb468
Mohamed-bek <mohamedbekkouche0302@gmail.com>
2025-04-19 22:00:33 +0100

    add conversations

--- routes/MessageRouter.js
+++ routes/MessageRouter.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
-import { getMessages } from "../controllers/MessageController.js";
+import {
+  getMessages,
+  getConversations,
+} from "../controllers/MessageController.js";
 import { authenticateToken } from "../middlewares/auth.js";
 
 const messageRoutes = Router();
 
 messageRoutes.get("/:patientId", authenticateToken, getMessages);
+messageRoutes.get(
+  "/conversations/patients",
+  authenticateToken,
+  getConversations
+);
 
 export default messageRoutes;
